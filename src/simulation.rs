fe26955148a154f0ce31d389e7ef24ffd0ce4076
Daniel Barker <phuebu@googlemail.com>
2025-02-25 09:53:05 +0000

    use exec_kernel macro

--- src/simulation.rs
+++ src/simulation.rs
@@ -9,15 +9,8 @@ use rmp_serde::{Deserializer, Serializer};
 use serde::{Deserialize, Serialize};
 
 use crate::fields::{Fluid, FluidDeserializer, MemUsage, Scalar, ScalarDeserializer};
-use crate::opencl::{
-    CtxDeserializer, Data1dDeserializer, Data2dDeserializer, DataNd, DataNdDeserializer,
-};
-use crate::raster::IxLike;
-use crate::{
-    opencl::{Data, Data1d, Data2d, OpenCLCtx},
-    raster::Ix,
-    velocities::VelocitySet,
-};
+use crate::opencl::{CtxDeserializer, DataNd, DataNdDeserializer};
+use crate::opencl::{Data, Data2d, OpenCLCtx};
 
 /// Contains information about the cells.
 /// For example, whether they are a fluid or wall, have fixed velocity, etc.
@@ -193,12 +186,15 @@ impl Simulation {
 
         let queue: &CommandQueue = &self.opencl.queue;
 
-        // round global work size upto the next even multiple of 8
+        // Round global work size upto the next even multiple of 8.
         let count_s = self.cells.counts.mapv(|c| {
             let r = c % 16;
             c + (16 - r)
         });
         let count_s = count_s.as_slice().unwrap();
+
+        // I haven't extensively optimized this but 8x8 seems like a good value for 2D.
+        let wsize = [8, 8];
 
         // TODO: make this generic over [`D`], [`Q`] and [`EqnType`]!
         let d2q9 = &self.opencl.d2q9_ns_kernel;
@@ -240,45 +236,6 @@ impl Simulation {
             // Entry case.
             ($kernel:expr, $($args:expr),*) => {
                 _exec_kernel_inner!{ ExecuteKernel::new($kernel), $($args),*}
-                    .set_global_work_size(cell_count)
-                    .enqueue_nd_range(&queue)
-                    .expect("ExecuteKernel::new failed.")
-            };
-        }
-
-        // I haven't extensively optimized this but 8x8 seems like a good value for 2D.
-        let wsize = [8, 8];
-
-        for iter in 0..iters {
-            // We cannot pass bools to OpenCL so get an interger with values 0 or 1.
-            let even = (1 - iter % 2) as i32;
-
-            // let d2q9_event = unsafe {
-            //     exec_kernel!(
-            //         d2q9,
-            //         &even,
-            //         &self.omega_ns,
-            //         &self.gravity[0],
-            //         &self.gravity[1],
-            //         &mut self.fluid.f.dev,
-            //         &mut self.fluid.rho.dev,
-            //         &mut self.fluid.vel.dev,
-            //         &self.cells.typ.dev,
-            //         &self.cells.idx.dev
-            //     )
-            // };
-
-            let d2q9_event = unsafe {
-                ExecuteKernel::new(&d2q9)
-                    .set_arg(&even)
-                    .set_arg(&self.fluid.omega)
-                    .set_arg(&self.gravity[0])
-                    .set_arg(&self.gravity[1])
-                    .set_arg(&mut self.fluid.f.dev)
-                    .set_arg(&mut self.fluid.rho.dev)
-                    .set_arg(&mut self.fluid.vel.dev)
-                    .set_arg(&self.cells.typ.dev)
-                    .set_arg(&qs_d2q9.dev)
                     .set_arg(&(self.cells.counts[0] as i32))
                     .set_arg(&(self.cells.counts[1] as i32))
                     .set_local_work_sizes(&wsize)
@@ -286,26 +243,42 @@ impl Simulation {
                     .enqueue_nd_range(&queue)
                     .expect("ExecuteKernel::new failed.")
             };
+        }
+
+        for iter in 0..iters {
+            // We cannot pass bools to OpenCL so get an interger with values 0 or 1.
+            let even = (1 - iter % 2) as i32;
+
+            let d2q9_event = unsafe {
+                exec_kernel!(
+                    d2q9,
+                    &even,
+                    &self.fluid.omega,
+                    &self.gravity[0],
+                    &self.gravity[1],
+                    &mut self.fluid.f.dev,
+                    &mut self.fluid.rho.dev,
+                    &mut self.fluid.vel.dev,
+                    &self.cells.typ.dev,
+                    &qs_d2q9.dev
+                )
+            };
 
             events.push(d2q9_event.get());
             queue.finish().expect("queue.finish failed");
 
             for tracer in self.tracers.iter_mut() {
                 let d2q5_event = unsafe {
-                    ExecuteKernel::new(&d2q5)
-                        .set_arg(&even)
-                        .set_arg(&tracer.omega)
-                        .set_arg(&mut tracer.g.dev)
-                        .set_arg(&mut tracer.C.dev)
-                        .set_arg(&self.fluid.vel.dev)
-                        .set_arg(&self.cells.typ.dev)
-                        .set_arg(&qs_d2q5.dev)
-                        .set_arg(&(self.cells.counts[0] as i32))
-                        .set_arg(&(self.cells.counts[1] as i32))
-                        .set_local_work_sizes(&wsize)
-                        .set_global_work_sizes(count_s)
-                        .enqueue_nd_range(&queue)
-                        .expect("ExecuteKernel::new failed.")
+                    exec_kernel!(
+                        d2q5,
+                        &even,
+                        &tracer.omega,
+                        &mut tracer.g.dev,
+                        &mut tracer.C.dev,
+                        &self.fluid.vel.dev,
+                        &self.cells.typ.dev,
+                        &qs_d2q5.dev
+                    )
                 };
 
                 events.push(d2q5_event.get());
