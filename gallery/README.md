6f51f5af24ac696c4526295dfd3e04d86e17e534
Daniel Barker <phuebu@googlemail.com>
2025-02-28 21:40:54 +0000

    add jets example

--- gallery/README.md
+++ gallery/README.md
@@ -9,3 +9,13 @@ They were all run on my local desktop PC, which has an RTX 4060 Ti graphics card
 ![Flow Past a Cylinder](banner.png)
 
 2D simulation with 27 million grid cells at a Reynold's number of 10,000.
+Code at [`vortex_street/vortex_street.py`](python/examples/vortex_street/vortex_street.py).
+
+## Colliding Jets
+
+![Colliding jets with tracers](jets.png)
+
+An example simulation showing the use of tracers.
+The simulation is of two jets colliding head-on. 
+Each jet & its motion is highlighted by the tracers, here coloured yellow and purple for each jet.
+Code at [`jets/jets.py`](python/examples/jets/jets.py).
---
+++ python/examples/jets/jets.py
@@ -0,0 +1,55 @@
+# %%
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from boltzmann.core import Simulation, CellType
+
+np.random.seed(42)
+
+# Create the simulation
+tau = 0.52
+cnt = [1500, 1500]
+sim = Simulation("gpu", cnt, 1 / tau)
+red = sim.add_tracer(1 / tau)
+grn = sim.add_tracer(1 / tau)
+
+# Set some initial conditions
+n = 40
+nn = [cnt[0] // n, cnt[1] // n]
+sx = slice(cnt[0] // 3, cnt[0] // 3 + nn[0])
+sy = slice(cnt[1] // 2 - nn[1], cnt[1] // 2 + nn[1])
+sim.fluid.vel[sx, sy, 0] = 0.1
+red.val[sx, sy] = 1.0
+sim.cells.flags[sx, sy] |= CellType.FIXED_FLUID_VELOCITY.value
+sim.cells.flags[sx, sy] |= CellType.FIXED_SCALAR_VALUE.value
+
+sx = slice(2 * cnt[0] // 3 - nn[0], 2 * cnt[0] // 3)
+sy = slice(cnt[1] // 2 - nn[1], cnt[1] // 2 + nn[1])
+sim.fluid.vel[sx, sy, 0] = -0.1
+grn.val[sx, sy] = 1.0
+sim.cells.flags[sx, sy] |= CellType.FIXED_FLUID_VELOCITY.value
+sim.cells.flags[sx, sy] |= CellType.FIXED_SCALAR_VALUE.value
+
+sim.fluid.rho[:] += 0.01 * np.random.uniform(-1, 1, sim.fluid.rho.shape)
+
+# %%
+
+tprev = 0
+for i, tcurr in enumerate([3000, 10000, 15000, 20000]):
+    # Run it
+    sim.iterate(tcurr - tprev)
+    tprev = tcurr
+
+    # Plot it
+    rgb = np.zeros((*sim.fluid.rho.shape, 3))
+    rgb[..., 0] = red.val * 0.9 + grn.val * 0.7
+    rgb[..., 1] = red.val * 0.9
+    rgb[..., 2] = grn.val * 0.9
+    rgb = np.swapaxes(rgb, 0, 1)
+    plt.imshow(rgb, origin="lower", interpolation="none")
+    plt.axis("off")
+    plt.savefig(f"jets_{i}.png", dpi=500, bbox_inches="tight", pad_inches=0)
+    plt.show()
+
+# %%
