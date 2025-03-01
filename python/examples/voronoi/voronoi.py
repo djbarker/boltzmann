9642e46dac353cefa218b420987a3ac7ccc301f8
Daniel Barker <phuebu@googlemail.com>
2025-03-01 14:23:18 +0000

    update voronoi.py

--- python/examples/voronoi/voronoi.py
+++ python/examples/voronoi/voronoi.py
@@ -1,44 +1,59 @@
 # %% Imports
 
 import logging
+import matplotlib.pyplot as plt
+import matplotlib.patheffects as pe
 import numpy as np
 import os
 
-from pprint import pprint
-from dataclasses import asdict
+from pathlib import Path
 from scipy.signal import convolve2d
 from scipy.ndimage import distance_transform_edt
+from scipy.stats.qmc import PoissonDisk
 
-from boltzmann.utils.logger import tick, PerfInfo, basic_config
-from boltzmann.core import Domain, SimulationMeta, FluidMeta, D2Q9, CellType
+from boltzmann.utils.logger import basic_config, dotted, time
+from boltzmann.core import (
+    VELOCITY,
+    Domain,
+    Simulation,
+    CellType,
+    TimeMeta,
+    calc_lbm_params,
+    Scales,
+    ACCELERATION,
+)
+from boltzmann.simulation import parse_cli, run_sim
 
-log = logging.getLogger(__name__)
+np.random.seed(42)
 
-basic_config(log)
-
-log.info("Starting")
+basic_config()
+logger = logging.getLogger(__name__)
 
 
 def voronoi(
-    points: np.ndarray, extent: np.ndarray, counts: np.ndarray, width: float, upsample: int = 4
+    points: np.ndarray,
+    extent: np.ndarray,
+    counts: np.ndarray,
+    width: float,
 ) -> np.ndarray:
+    """
+    Generate a Voronoi diagram with a given width around the points.
+    """
     assert points.ndim == 2
     assert points.shape[1] == 2
     assert extent.shape == (2, 2)
     assert counts.shape == (2,)
 
-    counts_ = counts * upsample
+    size_x = extent[0, 1] - extent[0, 0]
+    size_y = extent[1, 1] - extent[0, 0]
 
-    II = (counts_[0] * (points[:, 0] - extent[0, 0]) / (extent[0, 1] - extent[0, 0])).astype(
-        np.int64
-    )
-    JJ = (counts_[1] * (points[:, 1] - extent[1, 0]) / (extent[1, 1] - extent[1, 0])).astype(
-        np.int64
-    )
+    idx_x = (counts[0] * (points[:, 0] - extent[0, 0]) / size_x).astype(np.int64)
+    idx_y = (counts[1] * (points[:, 1] - extent[1, 0]) / size_y).astype(np.int64)
 
-    WW = np.zeros(tuple(counts_))
-    WW[II, JJ] = 1
+    WW = np.zeros(tuple(counts))
+    WW[idx_x, idx_y] = 1
 
+    # Tile so the middle chunk looks periodic.
     WW = np.tile(WW, (3, 3))
 
     KK = np.array(
@@ -50,213 +65,172 @@ def voronoi(
     )
 
     DD = distance_transform_edt(1 - WW)
-    CC = convolve2d(DD, KK) > 0
+    CC = convolve2d(DD, KK, mode="same") > 0
 
     # TODO: downsample here
 
-    VV = distance_transform_edt(1 - CC) < (width * upsample)
+    # Here we add some noise to the width check to make the channels varied.
+    xx = np.linspace(extent[0, 0], extent[0, 1], counts[0])
+    yy = np.linspace(extent[1, 0], extent[1, 1], counts[1])
+    XX, YY = np.meshgrid(xx, yy)
+    K = [[3, 5], [6, -3], [7, 23]]  # wave number
+    A = [0.3, 0.2, 0.1]  # amplitude
+    WW = np.ones_like(XX) * 0.8
+    for k, a in zip(K, A):
+        WW += a * np.sin(2 * np.pi * (k[0] * XX / size_x + k[1] * YY / size_y))
+    WW = np.tile(WW, (3, 3)) * width
+
+    VV = distance_transform_edt(1 - CC) < (WW / 2)
 
     # chop out middle of periodic tiling
-    VV = VV[counts_[0] : 2 * counts_[0], counts_[1] : 2 * counts_[1]]
+    VV = VV[counts[0] : 2 * counts[0], counts[1] : 2 * counts[1]]
 
     return VV
 
 
 # %% Params
 
-nu_si = 3e-5
-rho_si = 1000
-mu_si = nu_si * rho_si
+x_si = 1.0  # domain size [m]
+w_si = 0.04  # channel width [m]
+g_si = 0.3  # Body acceleration [m/s^2]
+nu_si = 1e-4  # kinematic viscosity [m^2/s]
 
-cs_si = 1.0
+L = 1500  # domain width [cells]
 
-dom = Domain.with_extent_and_counts(extent_si=[[-0.1, 0.1], [-0.1, 0.1]], counts=[751, 751])
-fld = FluidMeta(mu_si, rho_si)
-sim = SimulationMeta.with_cs(domain=dom, fluid=fld, cs=cs_si)
+# implied quantities
+u_si = (1 / 8) * (g_si / nu_si) * w_si**2  # flow velocity [m/s]
+Re = int(u_si * w_si / nu_si)  # Reynolds number [1]
+D = L * (w_si / x_si)  # channel width [cells]
 
-log.info(f"\n{pprint(asdict(sim), sort_dicts=False, width=18)}")
+(tau, u) = calc_lbm_params(Re, D, tau=0.505, M_max=0.1)
+dx = x_si / L
+dt = (u / x_si) * dx
 
-n = 100
-out_dt_si = sim.dt * n
+dotted(logger, "Reynolds number", Re)
+dotted(logger, "tau", tau)
+dotted(logger, "u", u)
+dotted(logger, "L", L)
+dotted(logger, "dx", dx)
+dotted(logger, "dt", dt)
 
-# gravitational acceleration [m/s^2]
-g_si = 0.1
-g_lu = g_si * (sim.dt / sim.cs)
+scales = Scales.make(dx=dx, dt=dt)
 
-log.info(f"Steps per output: {n=}")
+# dimensionless time does not depend on viscosity, purely on distances
+out_dx_si = 0.5 * w_si  # want to output when flow has moved this far
+sim_dx_si = u_si * dt  # flow moves this far in dt (i.e. one iteration)
+n = out_dx_si / sim_dx_si
+n = int(n + 1e-8)
+out_dt_si = dt * n
+out_n = int(4 * x_si / out_dx_si)
 
+time_meta = TimeMeta.make(
+    dt_step=dt,
+    dt_output=out_dt_si,
+    output_count=out_n,
+)
 
-# %% Compile
+dotted(logger, "outputs", out_n)
 
-log.info("Compiling using Numba...")
-
-from bz_numba import *
-
-# make numba objects
-pidx = NumbaDomain(dom.counts)
-g_lu = np.array([0.0, -g_lu], dtype=np.float32)
-params = NumbaParams(sim.dt, dom.dx, sim.cs, sim.w_pos_lu, sim.w_neg_lu, g_lu)
+# geometry
+domain = Domain.make(counts=[L, L], dx=dx)
 
 # %% Initialize arrays
 
-f1 = make_array(pidx, 9)
-f2 = make_array(pidx, 9)
-vel_si = make_array(pidx, 2)
-rho_si = make_array(pidx, fill=fld.rho)
-cell = make_array(pidx, dtype=np.int32)
+omega_ns = 1 / tau
+omega_ad = 1 / 0.501
 
-np.random.seed(42)
-points = np.random.uniform(size=(100, 2))
-extent = np.array([[0, 1], [0, 1]])
-counts = np.array([750, 750])
+counts = domain.counts.astype(np.int32)
 
-VV = voronoi(points, extent, counts, 12, upsample=1)
+args = parse_cli()
 
-cell_ = unflatten(pidx, cell)
-cell_[1:-1, 1:-1] = 1 - VV
+# Either load the simulation or create it.
+if args.resume:
+    with time(logger, "Loading simulation"):
+        sim = Simulation.load_checkpoint(args.dev, str(args.base / "checkpoint.mpk"))
+        tracer = sim.get_tracer(0)
+else:
+    with time(logger, "Creating simulation"):
+        sim = Simulation(args.dev, counts, q=9, omega_ns=omega_ns)
+        tracer = sim.add_tracer(q=5, omega_ad=omega_ad)
 
+    # Set-up wall boundaries
+    p_disk = PoissonDisk(2, radius=2 * w_si, rng=np.random.default_rng(42))
+    points = p_disk.fill_space()
+    extent = np.array([[0, 1], [0, 1]])
+    VV = voronoi(points, extent, counts, D)
 
-# ensure cell types match across boundary
-pidx.copy_periodic(cell)
+    sim.cells.flags[:] |= 1 - VV
 
-# flag arrays
-is_wall = (cell == CellType.WALL.value).astype(np.int32)
-update_vel = (cell == CellType.FLUID.value).astype(np.int32)
+    # Set-up initial tracer
+    x = np.arange(0, L, 1)
+    XX, YY = np.meshgrid(x, x, indexing="ij")
+    # mask = VV & (XX <= L // 11) & (5 * L // 11 <= YY) & (YY <= 6 * L // 11)
+    # tracer.val[mask] = 1.0
+    # sim.cells.flags[mask] |= CellType.FIXED_SCALAR_VALUE.value
 
-# initial f is equilibrium for desired values of v and rho
-calc_equilibrium(vel_si, rho_si, f1, np.float32(params.cs_si), D2Q9)
-f2[:] = f1[:]
+    mask = VV & (YY <= L // 5)
+    tracer.val[mask] = 1.0
 
-
-# %% Define VTK output function
-
-from boltzmann.vtkio import VtiWriter
-
-
-def write_vti(
-    path: str,
-    vel_si: np.ndarray,
-    rho_si: np.ndarray,
-    curl_si: np.ndarray,
-    cell: np.ndarray,
-    f: np.ndarray,
-    pidx: NumbaDomain,
-    params: NumbaParams,
-    *,
-    save_f: bool = False,
-):
-    with VtiWriter(path, pidx) as writer:
-        writer.add_data("Velocity", vel_si, default=True)
-        writer.add_data("Density", rho_si)
-        writer.add_data("Vorticity", curl_si)
-        writer.add_data("CellType", cell)
-        if save_f:
-            writer.add_data("F", f)
+    # IMPORTANT: convert gravity lattice units before setting
+    g_lu = scales.to_lattice_units(g_si, **ACCELERATION)
+    sim.set_gravity(np.array([0.0, g_lu], np.float32))
 
 
-# %% Write PNGs
-
-import matplotlib as mpl
-
-mpl.use("Agg")
-import matplotlib.pyplot as plt
+# %% Define simulation output
 
 
-def write_png(
-    path: str,
-    vel: np.ndarray,
-    rho: np.ndarray,
-    vort: np.ndarray,
-    cell: np.ndarray,
-    show: bool = False,
-):
-    vmag = np.sqrt(np.sum(vel**2, axis=-1))
-    vmag = unflatten(pidx, vmag)[1:-1, 1:-1]
-    cell = unflatten(pidx, cell)[1:-1, 1:-1]
-    vmag = np.where(cell == 1, np.nan, vmag)
+def write_output(base: Path, iter: int):
+    cmap = plt.get_cmap("viridis")
+    cmap.set_bad("gray")
 
-    ar = (dom.extent[0, 1] - dom.extent[0, 0]) / (dom.extent[1, 1] - dom.extent[1, 0])
+    def _annotate(label: str):
+        ax = plt.gca()
+        ax.text(
+            25,
+            85,
+            label,
+            color="white",
+            path_effects=[pe.withStroke(linewidth=2, foreground="k")],
+            fontsize=15,
+        )
 
-    fig = plt.figure(figsize=(8, 8 / ar), facecolor="#252525")
-    ax1 = fig.add_subplot(1, 1, 1)
+    mask = (sim.cells.flags & CellType.WALL.value) > 0
 
-    ax1.imshow(vmag, vmin=0, vmax=0.04, cmap=plt.cm.inferno)
-
-    fnt = dict(
-        fontsize=18,
-        fontfamily="sans-serif",
-        fontstyle="italic",
-        fontweight="bold",
-    )
-
-    # ax1.annotate(f"Re = {int(re+1e-8):,d}", (40, 90), c="w", **fnt)
-
+    vmag = np.sqrt(np.sum(sim.fluid.vel**2, axis=-1))
+    vmag[mask] = np.nan
+    vmax = 0.9 * np.nanmax(vmag)
+    plt.imshow(vmag.T, cmap=cmap, interpolation="none", vmax=vmax)
+    _annotate("Velocity")
     plt.axis("off")
+    plt.savefig(base / "vmag.png", bbox_inches="tight", pad_inches=0, dpi=300)
+    plt.close()
 
-    if not show:
-        fig.savefig(path, dpi=200, bbox_inches="tight", pad_inches=0)
-        fig.clear()
-        plt.close()
+    conc = tracer.val.copy()
+    conc[mask] = np.nan
+    plt.imshow(conc.T, cmap=cmap, interpolation="none", vmin=0.0, vmax=0.9)
+    _annotate("Tracer")
+    plt.axis("off")
+    plt.savefig(
+        base / f"conc_{iter:06d}.png", bbox_inches="tight", pad_inches=0, dpi=300
+    )
+    plt.close()
+
+    cmap = plt.get_cmap("inferno")
+    cmap.set_bad("gray")
+
+    rho = sim.fluid.rho.copy()
+    rho[mask] = np.nan
+    plt.imshow(rho.T, cmap=cmap, interpolation="none")
+    _annotate("Pressure")
+    plt.axis("off")
+    plt.savefig(base / "density.png", bbox_inches="tight", pad_inches=0, dpi=300)
+    plt.close()
+
+    print(scales.to_lattice_units(u_si, **VELOCITY), np.nanmax(vmag))
 
 
-# %% Main Loop
+# %% Run the simulation
 
-import time
-
-path_out = f"out/"
-tmpl_out = f"{path_out}/out_{{:06d}}.vti"
-
-path = os.path.dirname(tmpl_out)
-if not os.path.exists(path):
-    os.makedirs(path)
-
-
-# write out the zeroth timestep
-curl_si = calc_curl_2d(pidx, vel_si, cell, params.dx_si)
-# write_vti(tmpl_out.format(0), vel_si, rho_si, curl_si, cell, f1, pidx, params)
-write_png(f"{path_out}/out_{0:06d}.png", vel_si, rho_si, curl_si, cell)
-
-t = 0.0
-out_i = 1
-out_t = out_dt_si
-max_i = 50
-
-batch_i = int((out_dt_si + 1e-8) // sim.dt)
-
-log.info(f"{batch_i:,d} iters/output")
-
-perf_total = PerfInfo()
-
-for out_i in range(1, max_i):
-    perf_batch = tick()
-
-    if np.any(~np.isfinite(f1)):
-        raise ValueError(f"Non-finite value in f.")
-
-    loop_for_2(batch_i, vel_si, rho_si, f1, f2, is_wall, update_vel, params, pidx, D2Q9)
-
-    perf_batch = perf_batch.tock(events=np.prod(dom.counts) * batch_i)
-    perf_total = perf_total + perf_batch
-
-    mlups_batch = perf_batch.events / (1e6 * perf_batch.seconds)
-    mlups_total = perf_total.events / (1e6 * perf_total.seconds)
-
-    curl_si = calc_curl_2d(pidx, vel_si, cell, params.dx_si)
-    # write_vti(tmpl_out.format(out_i), vel_si, rho_si, curl_si, cell, f1, pidx, params)
-    write_png(f"{path_out}/out_{out_i:06d}.png", vel_si, rho_si, curl_si, cell)
-
-    if out_i % 10 == 0:
-        write_vti(f"{path_out}/checkpoint_vtk.vti", vel_si, rho_si, curl_si, cell, f1, pidx, params)
-        np.save(f"{path_out}/checkpoint_f1", f1, allow_pickle=False)
-        with open(f"{path_out}/checkpoint_info.txt", "w") as fout:
-            fout.write(f"{out_i=}")
-
-    # just to be nice to my cpu fan
-    if out_i % 10 == 0:
-        time.sleep(30)
-
-    vmax_si = np.max(np.sqrt(np.sum(vel_si**2, -1)))
-
-    log.info(f"{out_i=}, {out_t=:.3f} s, {mlups_batch:.2f} MLU/s, {vmax_si=:.4f} m/s\r")
-
-    out_t += out_dt_si
+run_sim(
+    args.base, time_meta, sim, write_output, write_checkpoints=not args.no_checkpoint
+)
