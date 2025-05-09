7b300d514c2eb95d5dda34509eca198f80a5d1bc
Daniel Barker <phuebu@googlemail.com>
2025-05-09 14:15:47 +0100

    update poiseuille

--- python/examples/poiseuille/poiseuille.py
+++ python/examples/poiseuille/poiseuille.py
@@ -10,14 +10,8 @@ from pathlib import Path
 from dataclasses import dataclass, field
 
 from boltzmann.utils.logger import basic_config, dotted
-from boltzmann.core import (
-    CellFlags,
-    SimulationMeta,
-    check_lbm_params,
-)
-from boltzmann.simulation import IterInfo, run_sim_cli
-from boltzmann.utils.vtkio import VtiWriter
-from boltzmann_rs import Simulation
+from boltzmann.core import CellFlags, calc_lbm_params_si, Simulation, bgk, trt
+from boltzmann.simulation import IterInfo, NoCheckpoints, run_sim
 
 basic_config()
 
@@ -50,11 +44,10 @@ rho_si = 1000
 mu_si = 0.001
 
 # Desired Reynold's number
-Re = 1000
+Re = 100
 
 # choose parameterisation
-L = 200
-tau = 0.51
+L = 100
 
 # implied acceleration & pressure drop
 g_si = 8 * Re * (mu_si / rho_si) ** 2 / L_si**3
@@ -64,8 +57,8 @@ dP_si = rho_si * g_si
 nu_si = mu_si / rho_si
 u_si = (g_si / nu_si) * (L_si**2) / 8
 Re = u_si * L_si / nu_si
-# T_si = 25 * L_si / u_si
-T_si = np.log(2) * L_si**2 / (np.pi**2 * nu_si)
+T_si = 25 * L_si / u_si
+# T_si = np.log(2) * L_si**2 / (np.pi**2 * nu_si)
 
 logger.info(f"Max velocity ............ {u_si} m/s")
 logger.info(f"Characteristic time ..... {T_si} s")
@@ -73,32 +66,23 @@ logger.info(f"Reynolds number ......... {Re:,f}")
 logger.info(f"Pressure drop ........... {dP_si / 1000} kPa/m")
 logger.info(f"Body acceleration ....... {g_si} m/s^2")
 
-nu = (tau - 0.5) / 3
-u = Re * nu / L
 dx = L_si / L
+tau, u = calc_lbm_params_si(dx, u_si, L_si, nu_si)
 dt = (u / u_si) * dx
 
 logger.info(f"dx ...................... {dx}")
 logger.info(f"dt ...................... {dt}")
 logger.info(f"u_LU .................... {u:.4f}")
 logger.info(f"L_LU .................... {L:.1f}")
-logger.info(f"nu_LU ................... {nu:.4f}")
 logger.info(f"tau_LU .................. {tau:.3f}")
 
-check_lbm_params(L, u, tau, M_max=0.3)
-
 domain = Domain.make(upper=[L_si, L_si / 4], dx=dx)
-scales = Scales.make(dx=dx, dt=dt)
-time = IterInfo.make(
+scales = Scales(dx, dt)
+iters = IterInfo.make(
     dt=dt,
     dt_output=T_si,
     t_max=T_si * 25,
 )
-meta = SimulationMeta(domain, time)
-
-logger.info(f"Cell counts ............. {tuple(domain.counts)}")
-logger.info(f"Iters / output .......... {time.batch_steps}")
-logger.info(f"Reynolds number ......... {Re:,.0f}")
 
 g_lu = scales.acceleration.to_lattice_units(g_si)
 g_lu = np.array([0, g_lu], dtype=np.float32)
@@ -107,93 +91,68 @@ g_lu = np.array([0, g_lu], dtype=np.float32)
 
 logger.info("Initializing arrays")
 
+omega = trt(tau)
+# omega = trt(tau, tau)
+# omega = bgk(tau)
+
 cnt = domain.counts.astype(np.int32)
-sim = Simulation(cnt, q=9, omega_ns=1 / tau)
+sim = Simulation("gpu", cnt, omega)
 
-cells_ = domain.unflatten(sim.domain.cell_type)
-cells_[+0, :] = CellFlags.WALL
-cells_[-1, :] = CellFlags.WALL
+sim.cells.flags[+0, :] = CellFlags.WALL
+sim.cells.flags[-1, :] = CellFlags.WALL
 
-vel_ = domain.unflatten(sim.fluid.vel)
-vel_[:, :, 0] = ((g_si / (2 * nu_si)) * domain.x * (L_si - domain.x))[:, None]
-vel_[:] = scales.velocity.to_lattice_units(vel_)
-
-mem_mb = (sim.domain.size_bytes + sim.fluid.size_bytes) / 1e6
-logger.info(f"Memory usage: {mem_mb:,.2f} MB")
+sim.fluid.vel[:, :, 0] = (g_si / (2 * nu_si)) * domain.xx * (L_si - domain.xx)
+sim.fluid.vel[:] = scales.velocity.to_lattice_units(sim.fluid.vel)
 
 sim.set_gravity(g_lu)
 
-# Important!
-sim.finalize(True)
 
-# %% Define simulation loop
+# %% Run simulation loop
 
+for iter in run_sim(sim, iters, "out", checkpoints=NoCheckpoints()):
+    # Generate output plots:
 
-@dataclass
-class PlanePoiseuille:
-    vti: bool = field(default=False)
+    fig = plt.figure(figsize=(6, 3))
+    ax = fig.gca()
+    plt.minorticks_on()
+    ax.grid(True, which="both", linewidth=0.5, color="#CCCCCC")
 
-    def loop_for(self, steps: int):
-        if np.any(~np.isfinite(sim.fluid.f)):
-            raise ValueError("Non-finite value in f.")
+    # Steady state.
+    u = (1 / 2) * (g_si / nu_si) * domain.x * (L_si - domain.x)
+    ax.plot(100 * domain.x, 100 * u, "k--", linewidth=1.5, label="Analytical")
 
-        sim.iterate(steps)
+    # Transient.
+    # u = u_analytical(domain.x, step * time.dt_output, L_si, g_si, nu_si)
+    # ax.plot(100 * domain.x, 100 * u, "g--", linewidth=1.5, label="Analytical")
 
-        logger.info(f"u_max = {100 * np.max(np.sum(vel_**2, axis=-1)) / u_si:.2f} %")
+    # Simulation.
+    u = scales.velocity.to_physical_units(sim.fluid.vel)
+    u = np.sqrt(np.sum(u**2, axis=-1))[:, 10]
+    ax.plot(
+        100 * domain.x,
+        100 * u,
+        linewidth=2.5,
+        c="#7d39aa",
+        alpha=0.8,
+        label="Simulation",
+    )
 
-    def write_output(self, base: Path, step: int):
-        # assert vel_.base is sim.fluid.vel
+    ax.legend(edgecolor="none")
+    # ax.set_ylim(ymin=0)
+    # ax.set_xlim(0, 100 * L_si)
+    ax.set_xlabel(r"$y$ [cm]")
+    ax.set_ylabel(r"$u(y)$ [cm/sec]")
+    plt.tight_layout()
+    plt.savefig(f"out/velprof_{iter:06d}.png", dpi=200)
+    fig.clear()
+    plt.close()
 
-        fig = plt.figure(figsize=(6, 3))
-        ax = fig.gca()
-        plt.minorticks_on()
-        ax.grid(True, which="both", linewidth=0.5, color="#CCCCCC")
+    if False:
+        with VtiWriter(str(f"out/data_{iter:06d}.vti"), domain) as writer:
+            writer.add_data("density", sim.fluid.rho)
+            writer.add_data("velocity", sim.fluid.vel * scales.dx / scales.dt, default=True)
+            writer.add_data("cell", sim.domain.cell_type)
 
-        # Steady state.
-        u = (1 / 2) * (g_si / nu_si) * domain.x * (L_si - domain.x)
-        ax.plot(100 * domain.x, 100 * u, "k--", linewidth=1.5, label="Analytical")
-
-        # Transient.
-        # u = u_analytical(domain.x, step * time.dt_output, L_si, g_si, nu_si)
-        # ax.plot(100 * domain.x, 100 * u, "g--", linewidth=1.5, label="Analytical")
-
-        # Simulation.
-        u = np.sqrt(np.sum(scales.velocity.to_physical_units(vel_) ** 2, axis=-1))[:, 1]
-        ax.plot(
-            100 * domain.x,
-            100 * u,
-            linewidth=2.5,
-            c="#7d39aa",
-            alpha=0.8,
-            label="Simulation",
-        )
-
-        ax.legend(edgecolor="none")
-        # ax.set_ylim(ymin=0)
-        # ax.set_xlim(0, 100 * L_si)
-        ax.set_xlabel(r"$y$ [cm]")
-        ax.set_ylabel(r"$u(y)$ [cm/sec]")
-        plt.tight_layout()
-        plt.savefig(base / f"velprof_{step:06d}.png", dpi=200)
-        fig.clear()
-        plt.close()
-
-        if self.vti:
-            with VtiWriter(str(base / f"data_{step:06d}.vti"), domain) as writer:
-                writer.add_data("density", sim.fluid.rho)
-                writer.add_data("velocity", sim.fluid.vel * scales.dx / scales.dt, default=True)
-                writer.add_data("cell", sim.domain.cell_type)
-
-    def write_checkpoint(self, base: Path):
-        pass
-
-    def read_checkpoint(self, base: Path):
-        pass
-
-
-# %% Main Loop
-
-run_sim_cli(meta, PlanePoiseuille(vti=False))
 
 # %% Save final output
 
